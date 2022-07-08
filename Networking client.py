e18881d6e49ac004f5b24aa96ff93b82e0f213eb
Danish Khan <83022701+danishkhanbx@users.noreply.github.com>
2022-07-08 14:51:50 +0530

    Delete Networking client.py

--- Networking client.py
+++
@@ -1,7 +0,0 @@
-import socket
-
-c = socket.socket()
-c.connect(('localhost', 9999))  # server will bind the port, client will simply connect(ip address, port number) with it
-name = input('Enter your name: ')
-c.send(bytes(name, 'utf-8'))  # sending clients name in utf format
-print(c.recv(1024).decode())
